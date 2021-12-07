8e2d13b4e8cb9d8a8e573d357e8328a34b2505e1
毛志浩 <85991202+zhdotm@users.noreply.github.com>
2021-12-07 14:50:05 +0800

    fix: 无效import

--- grape-starter/grape-starter-spring/src/main/java/com/mzh/grape/starter/spring/config/GrapeConfig.java
+++ grape-starter/grape-starter-spring/src/main/java/com/mzh/grape/starter/spring/config/GrapeConfig.java
@@ -7,7 +7,6 @@ import com.mzh.grape.starter.spring.runner.AutoAssemblingRunner;
 import com.mzh.grape.starter.spring.support.StateMachineSupport;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
